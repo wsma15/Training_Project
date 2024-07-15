03ab0c3a90dfa7833fde462e8a43870b6ba75831
Hussein Alwisi <hussein.alwisi@outlook.com>
2024-07-15 12:56:18 +0300

    issue : function doesnt register or called

--- TrainingApp/Controllers/AccountController.cs
+++ TrainingApp/Controllers/AccountController.cs
@@ -277,9 +277,46 @@ namespace TrainingApp.Controllers
         {
             if (ModelState.IsValid)
             {
-                // Register student logic
-                return RedirectToAction("Index", "Home");
+                try
+                {
+                    using (var context = new TrainingAppDBContext())
+                    {
+                        var companyName = context.Users
+                                                 .Where(u => u.Id == model.CompanySupervisorID)
+                                                 .Select(u => u.CompanyName)
+                                                 .FirstOrDefault();
+                        var uniName = context.Users
+                                             .Where(u => u.Id == model.UniversitySupervisorID)
+                                             .Select(u => u.UniversityName)
+                                             .FirstOrDefault();
+
+                        var user = new Users
+                        {
+                            Name = model.TrainerName,
+                            Email = model.TrainerEmail,
+                            Password = model.TrainerPassword, // Hash the password before saving
+                            UniversitySupervisorID = model.UniversitySupervisorID,
+                            CompanySupervisorID = model.CompanySupervisorID,
+                            CompanyName = companyName,
+                            Roles = UserRole.Trainer,
+                            UniversityName = uniName,
+                        };
+
+                        context.Users.Add(user);
+                        context.SaveChanges();
+                    }
+
+                    TempData["SuccessMessage"] = "Student registered successfully!";
+                    return RedirectToAction("RegistrationSuccess"); // Redirect to a success page or message
+                }
+                catch (Exception ex)
+                {
+                    // Log the exception
+                    ModelState.AddModelError("", "An error occurred while registering the student.");
+                }
             }
+
+            // Return to the registration view with the combined model if the model is not valid or an exception occurred
             var combinedModel = new CombinedRegistrationViewModel
             {
                 StudentViewModel = model,
@@ -294,9 +331,34 @@ namespace TrainingApp.Controllers
         {
             if (ModelState.IsValid)
             {
-                // Register company supervisor logic
-                return RedirectToAction("Index", "Home");
+                try
+                {
+                    using (var context = new TrainingAppDBContext())
+                    {
+                        var user = new Users
+                        {
+                            Name = model.FullName,
+                            CompanyName = model.CompanyName,
+                            Email = model.Email,
+                            Password = model.Password, // Hash the password before saving
+                            Roles = UserRole.CompanySupervisor
+                        };
+
+                        context.Users.Add(user);
+                        context.SaveChanges();
+                    }
+
+                    TempData["SuccessMessage"] = "Company Supervisor registered successfully!";
+                    return RedirectToAction("RegistrationSuccess");
+                }
+                catch (Exception ex)
+                {
+                    // Log the exception
+                    ModelState.AddModelError("", "An error occurred while registering the company supervisor.");
+                }
             }
+
+            // Return to the registration view with the combined model if the model is not valid or an exception occurred
             var combinedModel = new CombinedRegistrationViewModel
             {
                 StudentViewModel = new AddTrainerViewModel(),
@@ -311,9 +373,34 @@ namespace TrainingApp.Controllers
         {
             if (ModelState.IsValid)
             {
-                // Register university supervisor logic
-                return RedirectToAction("Index", "Home");
+                try
+                {
+                    using (var context = new TrainingAppDBContext())
+                    {
+                        var user = new Users
+                        {
+                            Name = model.FullName,
+                            UniversityName = model.UniversityName,
+                            Email = model.SupervisorEmail,
+                            Password = model.SupervisorPassword, // Hash the password before saving
+                            Roles = UserRole.UniversitySupervisor
+                        };
+
+                        context.Users.Add(user);
+                        context.SaveChanges();
+                    }
+
+                    TempData["SuccessMessage"] = "University Supervisor registered successfully!";
+                    return RedirectToAction("RegistrationSuccess");
+                }
+                catch (Exception ex)
+                {
+                    // Log the exception
+                    ModelState.AddModelError("", "An error occurred while registering the university supervisor.");
+                }
             }
+
+            // Return to the registration view with the combined model if the model is not valid or an exception occurred
             var combinedModel = new CombinedRegistrationViewModel
             {
                 StudentViewModel = new AddTrainerViewModel(),
@@ -324,35 +411,6 @@ namespace TrainingApp.Controllers
         }
 
         //
-        // POST: /Account/Register
-        [HttpPost]
-        [AllowAnonymous]
-        [ValidateAntiForgeryToken]
-        public async Task<ActionResult> Register(RegisterViewModel model)
-        {
-            if (ModelState.IsValid)
-            {
-                var user = new ApplicationUser { UserName = model.Email, Email = model.Email };
-                var result = await UserManager.CreateAsync(user, model.Password);
-                if (result.Succeeded)
-                {
-                    await SignInManager.SignInAsync(user, isPersistent: false, rememberBrowser: false);
-
-                    // For more information on how to enable account confirmation and password reset please visit https://go.microsoft.com/fwlink/?LinkID=320771
-                    // Send an email with this link
-                    // string code = await UserManager.GenerateEmailConfirmationTokenAsync(user.Id);
-                    // var callbackUrl = Url.Action("ConfirmEmail", "Account", new { userId = user.Id, code = code }, protocol: Request.Url.Scheme);
-                    // await UserManager.SendEmailAsync(user.Id, "Confirm your account", "Please confirm your account by clicking <a href=\"" + callbackUrl + "\">here</a>");
-
-                    return RedirectToAction("Index", "Home");
-                }
-                AddErrors(result);
-            }
-
-            // If we got this far, something failed, redisplay form
-            return View(model);
-        }
-
         //
         // GET: /Account/ConfirmEmail
         [AllowAnonymous]
