c7783259024be58598f1ea654e00c5d924ab5a89
Hussein Alwisi <hussein.alwisi@outlook.com>
2024-08-20 12:51:36 +0300

    .

--- TrainingApp/js/script.js
+++ TrainingApp/js/script.js
@@ -1,13 +1,6 @@
-/*------------------------------------------------------------------
-* Bootstrap Simple Admin Template
-* Version: 3.0
-* Author: Alexis Luna
-* Website: https://github.com/alexis-luna/bootstrap-simple-admin-template
--------------------------------------------------------------------*/
 (function() {
     'use strict';
 
-    // Toggle sidebar on Menu button click
     $('#sidebarCollapse').on('click', function() {
         $('#sidebar').toggleClass('active');
         $('#body').toggleClass('active');
